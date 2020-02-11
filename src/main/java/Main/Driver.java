ad385549f7017fc025d050722ff4a6804858a23d
Regulator1904 <ordonezemanuel51@gmail.com>
2020-02-11 08:11:11 -0800

    update to constructor, attributes, and function operations

--- src/main/java/Main/Driver.java
+++ src/main/java/Main/Driver.java
@@ -5,11 +5,14 @@ import Task.*;
 
 public class Driver {
     public static void main(String[] args) {
+        
         Owner craig = new Owner("Craig", "####");
+        Company company = new Company("Cis","######");
+        System.out.println("Company: "+ company.getName());
         System.out.println("Created new owner: " + craig.getName());
         // creates new employees, can change to iterate through a list of names and a for loop
-        Manager john = new Manager("John");
-        Manager mary = new Manager("Mary");
+        Manager john = new Manager("John","##");
+        Manager mary = new Manager("Mary","##");
 
         System.out.println("Created new managers: " + john.getName() + " and " + mary.getName());
 
